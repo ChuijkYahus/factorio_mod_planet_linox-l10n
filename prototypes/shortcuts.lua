dcca3bd678a3540ffafe0813841eba9b3e27842f
Xeon <xeon0527@naver.com>
2026-08-12 19:13:16 +0900

    1.8.6

--- prototypes/shortcuts.lua
+++ prototypes/shortcuts.lua
@@ -5,9 +5,10 @@ data:extend {
     action = "lua",
     associated_control_input = "toggle-driving",
     technology_to_unlock = "linox-technology_exploring-linox-landing-site",
-    icon = "__base__/graphics/icons/shortcut-toolbar/mip/copy-x56.png",
-    icon_size = 56,
-    small_icon = "__base__/graphics/icons/shortcut-toolbar/mip/copy-x24.png",
-    small_icon_size = 24
+
+    icon = "__linox__/graphics/icons/elevator.png",
+    icon_size = 64,
+    small_icon = "__linox__/graphics/icons/elevator.png",
+    small_icon_size = 64
   },
 }
